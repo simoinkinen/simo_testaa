b104efc86c120f61c8f62edcb468cff17c8758cc
simoinkinen <simo.inkinen@iki.fi>
2017-05-09 13:54:26 +0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+   public static void say(String message) {
+      System.out.println(message);
+   }
+
+   public static void main(String[] args) {
+      String message = "Hello World!";
+      say(message);
+   }
+}
